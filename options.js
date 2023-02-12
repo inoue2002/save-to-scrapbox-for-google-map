819a5a324279d2647249ed647e92b3b0fb89c2d1
inoue2002 <54356188+youkan2002@users.noreply.github.com>
2023-02-12 13:15:01 +0900

    cording option.js

--- options.js
+++ options.js
@@ -1,8 +1,35 @@
-document.getElementById('checkButton').addEventListener('click', async (e) => {
-  const projectName = document.getElementById('nameText').value;
+window.addEventListener('load', async () => {
+  await chrome.storage.sync.get(null, async (options) => {
+    const projectName = options['projectName'];
+    const googleApiKey = options['googleApiKey'];
+    const gyazoApiKey = options['gyazoApiKey'];
+    const isGetImage = options['isGetImage'];
+    const isDevMode = options['isDevMode'];
+    console.log({ projectName, googleApiKey, gyazoApiKey, isGetImage, isDevMode });
+    if (projectName) document.getElementById('projectName').value = projectName;
+    if (googleApiKey) document.getElementById('googleApiKey').value = googleApiKey;
+    if (gyazoApiKey) document.getElementById('gyazoApiKey').value = gyazoApiKey;
+    if (isGetImage) document.getElementById('isGetImage').checked = isGetImage;
+    if (isDevMode) document.getElementById('isDevMode').checked = isDevMode;
+  });
+});
+
+document.getElementById('saveButton').addEventListener('click', (/*e*/) => {
+  const projectName = document.getElementById('projectName').value;
+  const googleApiKey = document.getElementById('googleApiKey').value;
+  const gyazoApiKey = document.getElementById('gyazoApiKey').value;
+  const isGetImage = document.getElementById('isGetImage').checked;
+  const isDevMode = document.getElementById('isDevMode').checked;
   const options = {
     projectName,
+    googleApiKey,
+    gyazoApiKey,
+    isGetImage,
+    isDevMode
   };
   chrome.storage.sync.set(options);
-  document.querySelector('#msg').innerText = `Save to ProjectName : ${options.projectName} !`;
+  document.querySelector('#msg').innerText = '✅ Saved!';
+  setTimeout(() => {
+    document.querySelector('#msg').innerText = '';
+  }, 3000);
 });
